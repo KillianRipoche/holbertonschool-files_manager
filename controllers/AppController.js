857c286f73284177d1e383cd57825b50dda9d2c6
KillianRipoche <killian.ripoche@gmail.com>
2026-01-05 15:30:38 +0100

    pitié mr le checker

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,20 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export default class AppController {
-  static async getStatus(req, res) {
-    const redisStatus = await redisClient.isAlive();
-    const dbStatus = await dbClient.isAlive();
-
-    res.status(200).json({ redis: redisStatus, db: dbStatus });
+class AppController {
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).json(status);
   }
 
   static async getStats(req, res) {
-    const nUsers = await dbClient.nbUsers();
-    const nFiles = await dbClient.nbFiles();
-
-    res.status(200).json({ users: nUsers, files: nFiles });
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).json({ users, files });
   }
 }
+
+export default AppController;
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 import express from 'express';
 import routes from './routes/index';
 
-
-
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use('/', routes);
-
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
