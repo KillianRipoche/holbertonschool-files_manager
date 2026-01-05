fd1957c40b087a0b0da352ac233e0dbeefdf031e
KillianRipoche <killian.ripoche@gmail.com>
2026-01-05 14:39:57 +0100

    Reprise de mes fichiers :D

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import AppController from '../controllers/AppController.js';
+
+
+const router = express.Router();
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+
+export default router;
---
+++ utils/redis.mjs
@@ -0,0 +1,49 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.log(`Redis client not connected to the server: ${err}`);
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setexAsync = promisify(this.client.setex).bind(this.client);  // 👈 Utilise setex au lieu de set
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    try {
+      const value = await this.getAsync(key);
+      return value;
+    } catch (err) {
+      console.error('Error retrieve value:', err);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.setexAsync(key, duration, value);
+    } catch (err) {
+      console.error('Error setting value:', err);
+    }
+  }
+
+  async del(key) {
+    try {
+      await this.delAsync(key);
+    } catch (err) {
+      console.error('Error deleting value:', err);
+    }
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
