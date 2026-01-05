89004bc06fd7524e737d56c81e5c1e7b61579fa9
KillianRipoche <killian.ripoche@gmail.com>
2026-01-05 16:02:40 +0100

    Ajouter la méthode getMe pour récupérer les informations de l'utilisateur connecté

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,6 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -31,6 +32,36 @@ class UsersController {
       email,
     });
   }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({
+      _id: new dbClient.ObjectId(userId),
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({
+      id: user._id.toString(),
+      email: user.email,
+    });
+  }
 }
 
 export default UsersController;
+
